bcfe2b5e433c5b0abd370656012032596d5a2b06
Segundo <segundogaldames@gmail.com>
2018-03-12 09:15:26 -0300

    Cambio de parametros para heroku

--- application/Config.php
+++ application/Config.php
@@ -15,7 +15,7 @@ define('DB_NAME', 'prueba_lime');
 define('DB_CHAR', 'utf8');*/
 
 //config heroku
-define('BASE_URL', 'https://prueba-lime.herokuapp.com/');
+define('BASE_URL', 'https://plime.herokuapp.com/');
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
@@ -24,7 +24,7 @@ define('HASH_KEY', '548fec1t3212');
 define('CTRL', '5a7b07cda1026');
 
 define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
-define('DB_USER', 'b24c8765b6b4d3');
-define('DB_PASS', '8bf6fcfe');
-define('DB_NAME', 'heroku_df2a75f1b187f32');
+define('DB_USER', 'b6edc9aaacea90');
+define('DB_PASS', 'cc026ce5');
+define('DB_NAME', 'heroku_36a6884c2ca2262');
 define('DB_CHAR', 'utf8');
