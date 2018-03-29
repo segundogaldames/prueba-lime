9a4d32b1bbcda71fc353dae0cf2f2c1ebeb8cb39
Segundo <segundogaldames@gmail.com>
2018-03-29 17:05:58 -0300

    Configuracion y base de datos

--- application/Config.php
+++ application/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-/*define('BASE_URL', 'http://localhost:8080/prueba-lime/');
+define('BASE_URL', 'http://localhost:8080/prueba-lime/');
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
@@ -12,19 +12,6 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '1234');
 define('DB_NAME', 'prueba_lime');
-define('DB_CHAR', 'utf8');*/
-
-//config heroku
-define('BASE_URL', 'https://plime.herokuapp.com/');
-define('DEFAULT_CONTROLLER', 'index');
-define('DEFAULT_LAYOUT', 'default');
-
-define('SESSION_TIME', 10);
-define('HASH_KEY', '548fec1t3212');
-define('CTRL', '5a7b07cda1026');
-
-define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
-define('DB_USER', 'b6edc9aaacea90');
-define('DB_PASS', 'cc026ce5');
-define('DB_NAME', 'heroku_36a6884c2ca2262');
 define('DB_CHAR', 'utf8');
+
+
